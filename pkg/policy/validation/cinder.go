73166ea3112155cd66f2a245ab0050d2183a12a9
Mohammed Al-Dokimi <mnd7729@gmail.com>
2026-01-22 04:23:57 +0000

    Fixed policy validation generation

--- pkg/policy/validation/cinder.go
+++ pkg/policy/validation/cinder.go
@@ -2,7 +2,6 @@ package validation
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/OpenStack-Policy-Agent/OSPA/pkg/policy"
 )
@@ -22,101 +21,18 @@ func (v *CinderValidator) ValidateResource(check *policy.CheckConditions, resour
 	switch resourceType {
 
 	case "volume":
-		if err := validateAllowedChecks(check, []string{ "status", "age_gt", "unused", "exempt_names" }); err != nil {
+		if err := validateAllowedChecks(check, []string{"status", "age_gt", "unused", "exempt_names"}); err != nil {
 			return fmt.Errorf("rule %q: %w", ruleName, err)
 		}
-
 
 	case "snapshot":
-		if err := validateAllowedChecks(check, []string{ "status", "age_gt", "unused", "exempt_names" }); err != nil {
+		if err := validateAllowedChecks(check, []string{"status", "age_gt", "unused", "exempt_names"}); err != nil {
 			return fmt.Errorf("rule %q: %w", ruleName, err)
 		}
-
 
 	default:
 		return fmt.Errorf("rule %q: unsupported resource type %q for cinder service", ruleName, resourceType)
 	}
 
 	return nil
-}
-
-func validateAllowedChecks(check *policy.CheckConditions, allowed []string) error {
-	if len(allowed) == 0 {
-		return nil
-	}
-	allowedSet := make(map[string]bool, len(allowed))
-	for _, name := range allowed {
-		allowedSet[name] = true
-	}
-
-	if !hasAnyCheck(check, allowedSet) {
-		return fmt.Errorf("check must specify at least one of: %s", strings.Join(allowed, ", "))
-	}
-
-	disallowed := findDisallowedChecks(check, allowedSet)
-	if len(disallowed) > 0 {
-		return fmt.Errorf("check specifies unsupported fields: %s", strings.Join(disallowed, ", "))
-	}
-
-	return nil
-}
-
-func hasAnyCheck(check *policy.CheckConditions, allowed map[string]bool) bool {
-	for name := range allowed {
-		if isCheckSet(check, name) {
-			return true
-		}
-	}
-	return false
-}
-
-func findDisallowedChecks(check *policy.CheckConditions, allowed map[string]bool) []string {
-	var disallowed []string
-	for _, name := range []string{
-		"direction",
-		"ethertype",
-		"protocol",
-		"port",
-		"remote_ip_prefix",
-		"status",
-		"age_gt",
-		"unused",
-		"exempt_names",
-		"exempt_metadata",
-		"image_name",
-	} {
-		if isCheckSet(check, name) && !allowed[name] {
-			disallowed = append(disallowed, name)
-		}
-	}
-	return disallowed
-}
-
-func isCheckSet(check *policy.CheckConditions, name string) bool {
-	switch name {
-	case "direction":
-		return check.Direction != ""
-	case "ethertype":
-		return check.Ethertype != ""
-	case "protocol":
-		return check.Protocol != ""
-	case "port":
-		return check.Port != 0
-	case "remote_ip_prefix":
-		return check.RemoteIPPrefix != ""
-	case "status":
-		return check.Status != ""
-	case "age_gt":
-		return check.AgeGT != ""
-	case "unused":
-		return check.Unused
-	case "exempt_names":
-		return len(check.ExemptNames) > 0
-	case "exempt_metadata":
-		return check.ExemptMetadata != nil
-	case "image_name":
-		return len(check.ImageName) > 0
-	default:
-		return false
-	}
 }
